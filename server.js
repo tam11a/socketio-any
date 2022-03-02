06c62bc2666457e1f96b42f3bfd0f3f73c4ae657
tam11a <ibrahimsadiktamim@gmail.com>
2022-03-03 00:34:41 +0600

    cors updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:3000", "https://track-geo.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
