8d1f2aa4dedd5ef2a4eff7cf7dd3e956fee68698
tam11a <ibrahimsadiktamim@gmail.com>
2022-03-03 02:29:48 +0600

    sad

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
-/*
-{
+const io = require("socket.io")(server, {
   cors: {
     origin: ["http://localhost:3000", "http://track-geo.netlify.app", "*"],
     methods: ["GET", "POST"],
   },
-}
-*/
+});
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-/*app.use(
+app.use(
   cors({
     origin: ["*"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
   })
-);*/
+);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
