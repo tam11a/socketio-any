83087403eca77353c7cf39eecd11c406445a289c
tam11a <ibrahimsadiktamim@gmail.com>
2022-03-03 00:17:42 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+Array.prototype.remove = function () {
+  var what,
+    a = arguments,
+    L = a.length,
+    ax;
+  while (L && this.length) {
+    what = a[--L];
+    while ((ax = this.indexOf(what)) !== -1) {
+      this.splice(ax, 1);
+    }
+  }
+  return this;
+};
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "connected");
+
+  socket.onAny((eventName, data) => {
+    console.log(data);
+    io.emit(eventName, data);
+  });
+});
+
+const port = 4000;
+server.listen(process.env.PORT || port, () => {
+  console.log(`listening on *:${port}`);
+});
